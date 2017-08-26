9920a8b81c7b2318e8dbf35d5347907d69a504c7
Neonet39 <evgen.gonchar2016@yandex.ru>
2017-08-26 23:49:23 +0300

    DAO Layer and personal area

--- src/main/java/com/Controller/ControllerGeneralNumber.java
+++ src/main/java/com/Controller/ControllerGeneralNumber.java
@@ -1,5 +1,7 @@
 package com.Controller;
 
+import com.Dao.NumberDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,9 +13,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/number")
 public class ControllerGeneralNumber {
-
+    @Autowired
+    NumberDao numberDao;
     @RequestMapping(value = "/generating_numbers",method = RequestMethod.GET)
     public String generating_numbers(){
+
         return "generating_numbers";
     }
+
+    @RequestMapping(value = "/get_number",method = RequestMethod.GET)
+    public String get_number(){
+        return "generating_numbers";
+    }
+
 }
--- src/main/java/com/Controller/ControllerPersonal.java
+++ src/main/java/com/Controller/ControllerPersonal.java
@@ -1,8 +1,12 @@
 package com.Controller;
 
+import com.Service.AdminService.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Evgeny on 26.08.2017.
@@ -10,9 +14,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/personal")
 @Controller
 public class ControllerPersonal {
+    @Autowired
+    AdminService adminService;
 
     @RequestMapping(value = "/administrator_area",method = RequestMethod.GET)
-    public String administrator_area(){
-        return "administrator_area";
+    public ModelAndView administrator_area(){
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("number",adminService.getNumber());
+        modelAndView.setViewName("administrator_area");
+
+        return modelAndView;
     }
+
+    @RequestMapping(value = "/administrator_set_param",method = RequestMethod.GET)
+    public String administrator_set_param(@RequestParam("number") String number,
+                                 @RequestParam("metodNull") boolean metodNull){
+        if(number!=null) {
+            try {
+                int namberInt = Integer.valueOf(number);
+                adminService.setNumber(number);
+            }catch (Exception e){
+
+            }
+        }
+        if(metodNull)
+            adminService.method_null();
+
+        return "redirect:/personal/administrator_area";
+    }
+
 }
